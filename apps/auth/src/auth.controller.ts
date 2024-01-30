8b38c63a285a89f5dcf42dad4d483a8423a785e6
Julio Tavares <juliotavares197@gmail.com>
2024-01-29 22:01:35 -0300

    feat: make rabbitmq connection between microservices

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
@@ -8,5 +9,17 @@ export class AuthController {
   @Get()
   getHello(): string {
     return this.authService.getHello();
+  }
+
+  @MessagePattern({ cmd: 'get-user' })
+  async getUser(@Ctx() ctx: RmqContext) {
+    const channel = ctx.getChannelRef();
+    const message = ctx.getMessage();
+
+    channel.ack(message);
+
+    return {
+      user: 'USER',
+    };
   }
 }
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const USER = configService.get<string>('RABBITMQ_USER');
+  const PASSWORD = configService.get<string>('RABBITMQ_PASS');
+  const HOST = configService.get<string>('RABBITMQ_HOST');
+  const QUEUE = configService.get<string>('RABBITMQ_AUTH_QUEUE');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
+      noAck: false,
+      queue: QUEUE,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
 }
+
 bootstrap();
